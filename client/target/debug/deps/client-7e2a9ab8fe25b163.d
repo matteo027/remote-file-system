1597cb6e41bcc39f98c0bc1dbe98f28104192a32
Andrea Germano <89916898+andrea-germano@users.noreply.github.com>
2025-07-19 10:18:50 +0200

    removed client side  per machine file configuration from git

--- client/target/debug/deps/client-7e2a9ab8fe25b163.d
+++
@@ -1,5 +0,0 @@
-/home/matteo/Desktop/remote-file-system/client/target/debug/deps/client-7e2a9ab8fe25b163.d: src/main.rs
-
-/home/matteo/Desktop/remote-file-system/client/target/debug/deps/libclient-7e2a9ab8fe25b163.rmeta: src/main.rs
-
-src/main.rs:
--- client/target/debug/deps/client-c3e43eaefa11cb89.d
+++
@@ -1,5 +0,0 @@
-/home/matteo/Desktop/remote-file-system/client/target/debug/deps/client-c3e43eaefa11cb89.d: src/main.rs
-
-/home/matteo/Desktop/remote-file-system/client/target/debug/deps/libclient-c3e43eaefa11cb89.rmeta: src/main.rs
-
-src/main.rs:
