544d9c5c692b870daff425dce24ab26f8ea2ff66
matteo <matteoflores02@gmail.com>
2025-08-05 20:41:51 +0200

    Unmounting implemented

--- client/crates/rfs-cliApp/src/main.rs
+++ client/crates/rfs-cliApp/src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
-use fuser::MountOption;
+use fuser::{MountOption};
 use rfs_api::Server;
 use rfs_fuse::RemoteFS;
+use std::sync::{Arc, Condvar, Mutex};
 
 #[derive(Parser, Debug)]
 #[command(name = "Remote-FS", version = "0.1.0")]
@@ -15,14 +16,39 @@ struct Cli {
 
 fn main() {
     let cli = Cli::parse();
+
+    let mount_point = cli.mount_point.clone();
     let options = vec![
         MountOption::FSName("Remote-FS".to_string()),
         MountOption::RW,
     ];
-    eprintln!("Remote-FS mounted at {}", cli.mount_point);
+
+    let fs = RemoteFS::new(Server::new());
+
+    let session = fuser::spawn_mount2(fs, &mount_point, &options)
+        .expect("Failed to mount FUSE");
+
+    let pair = Arc::new((Mutex::new(false), Condvar::new()));
+    let pair_clone = pair.clone();
+
+    // Ctrl+C
+    ctrlc::set_handler(move || {
+        let (lock, cvar) = &*pair_clone;
+        let mut stop = lock.lock().unwrap();
+        *stop = true;
+        cvar.notify_one();
+        eprintln!("\nSignal received. Unmounting...");
+    })
+    .expect("Errore nel set_handler");
+
+    eprintln!("Remote-FS mounted on {}", cli.mount_point);
     eprintln!("Remote address: {}", cli.remote_address);
-    fuser::mount2(RemoteFS::new(Server::new()), cli.mount_point, &options)
-        .expect("failed to mount");
-    eprintln!("Remote-FS unmounted");
-    return;
+
+    // waits for the signal
+    let (lock, cvar) = &*pair;
+    let mut stop = lock.lock().unwrap();
+    stop = cvar.wait_while(stop, |s|{!*s}).expect("Mutex poisoned");
+
+    drop(session);
+    eprintln!("Remote-FS unmounted correctly");
 }
