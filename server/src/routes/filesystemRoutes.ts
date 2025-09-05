dfacc89f5932fa6d9de679eae6e4e3482b8a142e
andrea <andreager06@gmail.com>
2025-09-05 17:58:06 +0200

    updated routes

--- server/src/routes/filesystemRoutes.ts
+++ server/src/routes/filesystemRoutes.ts
@@ -1,28 +1,34 @@
 import { Router } from 'express';
 import express from 'express';
-import { FileSystemController } from '../controllers/filesystemController';
+import { FileController } from '../controllers/fileController';
+import { ReadWriteController } from '../controllers/RWController';
+import { AttributeController } from '../controllers/attrController';
 import { Express } from 'express-serve-static-core';
 import { AuthenticationController } from '../controllers/authenticationController';
 
 const router = Router();
-const filesystemController = new FileSystemController();
+const fileController = new FileController();
+const rwController = new ReadWriteController();
+const attrController = new AttributeController();
 const isLoggedIn = (new AuthenticationController).isLoggedIn;
 
 export function setRoutes(app: Express) {
     app.use('/', router);
 
-    router.get('/api/directories/:ino', isLoggedIn, filesystemController.readdir);
-    router.post('/api/directories/:parentIno', isLoggedIn, filesystemController.mkdir);
-    router.delete('/api/directories/:parentIno', isLoggedIn, filesystemController.rmdir);
+    router.get('/api/entries/:parentIno/:name', isLoggedIn, attrController.lookup);
 
-    router.patch('/api/files/attributes/{*path}', isLoggedIn, filesystemController.setattr);
-    router.get('/api/files/attributes/:ino', isLoggedIn, filesystemController.getattr);
+    router.get('/api/directories/:ino', isLoggedIn, attrController.readdir);
+    router.post('/api/directories/:parentIno', isLoggedIn, fileController.mkdir);
+    router.delete('/api/directories/:parentIno', isLoggedIn, fileController.rmdir);
 
-    router.post('/api/files/:parentIno', isLoggedIn, filesystemController.create);
-    router.put('/api/files/stream/{*path}', isLoggedIn, filesystemController.writeStream);
-    router.get('/api/files/stream/{*path}', isLoggedIn, filesystemController.readStream);
-    router.put('/api/files/{*path}', isLoggedIn, express.raw({type:'application/octet-stream', limit: '1gb'}), filesystemController.write);
-    router.get('/api/files/{*path}', isLoggedIn, filesystemController.read);
-    router.delete('/api/files/:parentIno', isLoggedIn, filesystemController.unlink);
-    router.patch('/api/files/:oldParentIno/:oldName', isLoggedIn, filesystemController.rename); // rename
+    router.patch('/api/files/attributes/{*path}', isLoggedIn, attrController.setattr);
+    router.get('/api/files/attributes/:ino', isLoggedIn, attrController.getattr);
+
+    router.post('/api/files/:parentIno', isLoggedIn, fileController.create);
+    router.put('/api/files/stream/{*path}', isLoggedIn, rwController.writeStream);
+    router.get('/api/files/stream/{*path}', isLoggedIn, rwController.readStream);
+    router.put('/api/files/{*path}', isLoggedIn, express.raw({type:'application/octet-stream', limit: '1gb'}), rwController.write);
+    router.get('/api/files/{*path}', isLoggedIn, rwController.read);
+    router.delete('/api/files/:parentIno', isLoggedIn, fileController.unlink);
+    router.patch('/api/files/:oldParentIno/:oldName', isLoggedIn, fileController.rename); // rename
 }
